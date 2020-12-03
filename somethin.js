84f79d985c8198145b3e3aa6b49360fba138f8d7
Annie Bourke <aabourke@gmail.com>
2020-12-03 22:31:14 +0000

    Automatically backed up by Learn

---
+++ somethin.js
@@ -0,0 +1,14 @@
+function changeState(state, action){      
+  switch (action.type) {
+    case 'INCREASE_COUNT':
+      return {count: state.count + 1}
+    case 'DECREASE_COUNT':
+      return {count: state.count - 1}
+    default:
+      return state;
+  }
+}
+
+let state = {count: 0}
+
+changeState(state, {type: 'INCREASE_COUNT'})
